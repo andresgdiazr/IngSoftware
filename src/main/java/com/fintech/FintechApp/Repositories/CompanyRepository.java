a253d3a50d88afefb1d1a0e76042d5e8d1c517da
oscarter19 <oscarg1910@gmail.com>
2024-06-15 00:20:37 -0400

    merge Nelson Oscar ramas

--- src/main/java/com/fintech/FintechApp/Repositories/CompanyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fintech.FintechApp.Repositories;
-
-import com.fintech.FintechApp.Models.Company;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CompanyRepository extends JpaRepository<Company,Long> {
-}
--- src/main/java/com/fintech/FintechApp/Repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fintech.FintechApp.Repositories;
-
-import com.fintech.FintechApp.Models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-}
